0c2a777ae143081c1b380291b376312bff554547
atman-33 <gpbjk0304@gmail.com>
2025-08-05 21:03:28 +0900

    feat: add google tag

--- app/components/ad-scripts.tsx
+++ app/components/ad-scripts.tsx
@@ -6,16 +6,17 @@ const AdScripts = () => {
         crossOrigin="anonymous"></script> */}
 
       {/* Google tag (gtag.js) */}
-      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=your_id"></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'your_id');
-        `}
-      </script> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-SVP41D6M1G"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SVP41D6M1G');
+          `,
+        }}
+      />
 
       {/* Adsterra Native Banner */}
       {/* <script async data-cfasync="false" src="your_js"></script> */}
